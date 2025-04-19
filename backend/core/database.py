05f8a472c4e8931f93e13a51515d6899ae681e7f
ANURAG SINGH BISEN <TNR-014@corp.ielektron.com>
2025-04-19 20:22:59 +0530

    newChanges

--- backend/core/database.py
+++ backend/core/database.py
@@ -7,7 +7,10 @@ DATABASE_URL = "postgresql://postgres:newpassword@localhost:5432/news"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 8ea485269136fbb5e78652377b9ae1267320136c
+=======
+>>>>>>> a87b6be (addedArticle)
 Base = declarative_base()
